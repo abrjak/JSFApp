67e63f5a9bb93c694eb29fefc650bc1e5ff1b676
Huuuuuiiiii <philipp.mueller@gibmit.ch>
2018-03-13 15:24:01 +0100

    une comment

--- JavaSource/ch/gibm/entity/Language.java
+++ JavaSource/ch/gibm/entity/Language.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-
+//Une Comment
 @Entity
 public class Language implements Serializable{
 	private static final long serialVersionUID = 1L;
