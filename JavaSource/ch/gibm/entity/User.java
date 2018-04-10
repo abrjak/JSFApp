74539eb9aad32213a3d3ebd7b2f16a031e9d6312
Jakob Abraham <abrjak01@hotmail.com>
2018-04-10 14:13:39 +0200

    10.04.2018: Created Login-Feature

--- JavaSource/ch/gibm/entity/User.java
+++ JavaSource/ch/gibm/entity/User.java
@@ -1,12 +1,19 @@
 package ch.gibm.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 
 @Entity
-public class User {
+@NamedQuery(name = "User.findUserByNameAndPassword", query = "select u from User u where u.userName = :userName and u.password = :password")
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	public static final String FIND_USER_BY_NAME_AND_PASSWORD = "User.findUserByNameAndPassword";
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
